f3eefaf3d4379fff20202f2389680e4ba4c2a3d7
a1-alex36 <a1-alex36@yandex.ru>
2021-03-06 18:24:29 +0300

    add form new user, page - /users/news

--- public/index.php
+++ public/index.php
@@ -27,7 +27,27 @@ $app->get('/', function ($request, $response) {
     // return $response->write('Welcome to Slim!');
 });
 
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['name' => '', 'email' => '', 'password' => '', 'passwordConfirmation' => '', 'city' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
 $users = ['mike', 'Mishel', 'adel', 'keks', 'kamila'];
+// построчно в массив из файла
+$file = __DIR__ . '/users.txt';
+$current = file_get_contents($file);
+$current = explode(PHP_EOL, $current);
+//print_r($current);
+$users2 = array_map(function ($user) {
+    //return json_decode($user)->name; // в объект
+    return json_decode($user, true)["name"]; // в массив
+}, $current);
+//print_r($users2);
+$users = [...$users, ...$users2];
+//print_r($users);
 $app->get('/users', function ($request, $response ) use ($users) {
     $name = $request->getQueryParam('name', '');
 
@@ -46,9 +66,32 @@ $app->get('/users', function ($request, $response ) use ($users) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
+//$repo = new App\UserRepository();
+
 $app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+    //$validator = new Validator();
+    $user = $request->getParsedBodyParam('user');
+    /*$errors = $validator->validate($user);
+    if (count($errors) === 0) {
+        $repo->save($user);
+        return $response->withRedirect('/users', 302);
+    }
+    */
+    //print_r($user);
+    $user = json_encode($user); //json_decode
+    $file = __DIR__ . '/users.txt';
+    // Открываем файл для получения существующего содержимого
+    $current = file_get_contents($file);
+    // Добавляем нового человека в файл
+    $current .= $user . "\n";
+    // Пишем содержимое обратно в файл
+    file_put_contents($file, $current);
+
+    return $response
+        ->withHeader('Location', '/users')
+        ->withStatus(302);
 });
+
 //$courses = ["mat", "lit"];
 $courses = [
     ["id" => 1,"name" => "math"],
@@ -67,7 +110,7 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
 });
-/*
+
 $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
@@ -75,5 +118,7 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     // $this в Slim это контейнер зависимостей
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
-*/
+
+
+
 $app->run();
